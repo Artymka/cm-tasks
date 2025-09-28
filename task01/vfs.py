58ebae2296c7e6070844a55a3b9b285ab2cb2635
Artymka <doomparser@gmail.com>
2025-09-28 18:04:04 +0300

    upd: implement vfs using classes

--- task01/vfs.py
+++ task01/vfs.py
@@ -1,67 +1,119 @@
+from __future__ import annotations
 import xml.etree.ElementTree as ET
 from typing import Dict
 import json
 import hashlib
 
 
-def read_vfs_form_xml(vfs_path: str) -> Dict:
-    """Читает данные о vfs из файла xml и возвращает в виде словаря со структурой:
-    'name': str - название файла/директории;
-    'type': file или dir;
-    'content': str - содержимое файла;
-    'children': [] - вложенные папки/файлы;
-    """
+class Node:
+    def __init__(self,
+                 name: str,
+                 type: str,
+                 parent: Node = None,
+                 content: str = ""):
+        self.name = name
+        self.type = type
+        self.parent = parent
+        self.content = content
+        self.children = []
 
-    tree = ET.parse(vfs_path)
-    root = tree.getroot()
+    def add_child(self, node: Node) -> None:
+        self.children.append(node)
+
+    def get_child(self, name: str) -> Node | None:
+        for child in self.children:
+            if child.name == name:
+                return child
+        return None
+
+    @classmethod
+    def read_vfs_form_xml(cls, vfs_path: str) -> Node:
+        """Читает данные о vfs из файла xml и возвращает в виде нода."""
+        tree = ET.parse(vfs_path)
+        root = tree.getroot()
+        
+        vfs_dict = cls._read_step(root)
+        return vfs_dict
     
-    vfs_dict = read_step(root)
-    return vfs_dict
+    @classmethod
+    def _read_step(cls, dir_inf: ET.Element, parent: Node = None) -> Node:
+        """Обрабатывает одну папку фалововой системы при чтении из файла."""
+        dir_node = Node(
+            dir_inf.attrib['name'],
+            "dir",
+            parent,
+        )
+        
+        for dir in dir_inf.findall('dir'):
+            # обработка всех папок, должна быть рекурсивной
+            dir_node.add_child(Node._read_step(dir, dir_node))
 
-def read_step(dir_inf: ET.Element) -> Dict:
-    """Обработка одной папки фалововой системы."""
-    dir_dict = {'name': dir_inf.attrib['name'], 'type': 'dir', 'children': []}
-
-    for dir in dir_inf.findall('dir'):
-        # обработка всех папок, должна быть рекурсивной
-        dir_dict['children'].append(read_step(dir))
-
-    for file in dir_inf.findall('file'):
-        # обработка файлов
-        dir_dict['children'].append({
-            'name': file.attrib['name'],
-            'type': 'file',
-            'content': file.attrib['content'],
-        })
+        for file in dir_inf.findall('file'):
+            # обработка файлов
+            dir_node.add_child(Node(
+                file.attrib['name'],
+                "file",
+                dir_node,
+                file.attrib['content']
+            ))
+        
+        return dir_node
     
-    return dir_dict
+    def _get_tree_root(self) -> Node:
+        """Возвращает корневой элемент дерева VFS."""
+        curr = self
+        while curr.parent:
+            curr = curr.parent
+        return curr
 
-def get_vfs_info(vfs_dict: Dict) -> str:
-    """Возвращает имя VFS, хеш SHA-256 объека в памяти (предварительно словарь преобразовывается в json с сортировкой ключей)."""
-    res = 'VFS info:\n'
-    res += 'VFS name: ' + vfs_dict['name'] + '\n'
-    dict_str = json.dumps(vfs_dict, sort_keys=True, ensure_ascii=False)
-    res += 'SHA-256: ' + hashlib.sha256(dict_str.encode('utf-8')).hexdigest() + '\n'
-    return res
+    def get_vfs_info(self) -> str:
+        """Возвращает имя VFS, хеш SHA-256 дерева в памяти (предварительно объект преобразуется в словарь, словарь преобразуется в json с сортировкой ключей)."""
+        root = self._get_tree_root()
+        # return root.to_dict()
+        res = 'VFS info:\n'
+        res += 'VFS name: ' + root.name + '\n'
+        dict_str = json.dumps(root.to_dict(), sort_keys=True, ensure_ascii=False)
+        res += 'SHA-256: ' + hashlib.sha256(dict_str.encode('utf-8')).hexdigest() + '\n'
+        return res
     
-def get_vfs_structure(vfs_dict: Dict) -> str:
-    """Возвращает структуру файлов и папок в vfs."""
-    res = ''
-    tab_size = 3
-    offset = [0]
-    stack = [vfs_dict]
-    while len(stack):
-        curr = stack.pop()
-        tabs = offset.pop()
+    def get_vfs_structure(self) -> str:
+        """Возвращает структуру файлов и папок в vfs."""
+        root = self._get_tree_root()
+        res = ''
+        tab_size = 3
+        offset = [0]
+        stack = [root]
+        while len(stack):
+            curr = stack.pop()
+            tabs = offset.pop()
 
-        res += ' ' * tabs * tab_size + curr['name'] + '\n'
-        for child in curr['children']:
-            if child['type'] == 'dir':
-                stack.append(child)
-                offset.append(tabs+1)
+            res += ' ' * tabs * tab_size + curr.name + '\n'
+            for child in curr.children:
+                if child.type == 'dir':
+                    stack.append(child)
+                    offset.append(tabs+1)
+                else:
+                    res += ' ' * (tabs+1) * tab_size + child.name + ': ' + child.content + '\n'
+        return res
+    
+    def to_dict(self) -> Dict:
+        """Возвращает всё дерево в виде словаря."""
+        root = self._get_tree_root()
+        res = self._to_dict_step(root)
+        return res
+        
+    def _to_dict_step(self, dir: Node) -> Dict:
+        """Преобразует одну директорию VFS в словарь."""
+        res = dir.__dict__.copy()
+        res["children"] = []
+        del(res["parent"])
+        for node in dir.children:
+            if node.type == "dir":
+                res['children'].append(self._to_dict_step(node))
             else:
-                res += ' ' * (tabs+1) * tab_size + child['name'] + ': ' + child['content'] + '\n'
-    return res
+                res['children'].append(node.__dict__.copy())
+                del(res['children'][-1]['parent'])
+        return res
 
 
 """
